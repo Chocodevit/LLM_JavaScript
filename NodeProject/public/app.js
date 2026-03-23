d49002c68ad443882884b358f763f79e96c7e87a
Chocodevit <2526_leonora.deleon@iticbcn.cat>
2026-03-23 22:14:54 +0100

    Setup project skeleton/scaffold,i nstall Node.js server with Express framework
    Descàrrega i instal·lació de Node.js (versió LTS) amb els complements necessaris (Python, etc.).
    Creació d’un projecte nou amb npm init -y per generar el package.json.
    Instal·lació d’Express com a dependència amb npm install express.
    Creació de l’arxiu index.js i prova bàsica amb console.log('hola mundo').
    Baixa i descompressió de l’esquelet del projecte.
    Obertura del projecte a Visual Studio i execució del servidor amb node server.js per fer el primer test

---
+++ NodeProject/public/app.js
@@ -0,0 +1,61 @@
+
+const btn = document.getElementById("btn");
+const btnToXML = document.getElementById("btnToXML");
+
+btn.addEventListener("click", async () => {
+
+  const text = document.getElementById("input").value;
+
+  // Fem una petició HTTP al servidor (Express)
+  // fetch() envia una request al backend
+  const res = await fetch("/convert", {
+    // Tipus de petició
+    // POST = enviem dades al servidor
+    method: "POST",
+    // Capçaleres HTTP
+    // Indiquem que estem enviant dades en format JSON
+    headers: {
+      "Content-Type": "application/json"
+    },
+
+    // Cos de la petició (les dades que enviem)
+    // Convertim l’objecte JS a text JSON
+    body: JSON.stringify({ data: text })
+  });
+
+  // El servidor respon amb JSON
+  // Convertim la resposta a objecte JavaScript
+  const json = await res.json();
+  
+  // Mostrem el resultat a la textarea de sortida
+  document.getElementById("output").value = json.result;
+});
+
+btnToXML.addEventListener("click", async () => {
+
+  const text = document.getElementById("input").value;
+
+  // Fem una petició HTTP al servidor (Express)
+  // fetch() envia una request al backend
+  const res = await fetch("/convertTOXMl", {
+    // Tipus de petició
+    // POST = enviem dades al servidor
+    method: "POST",
+    // Capçaleres HTTP
+    // Indiquem que estem enviant dades en format JSON
+    headers: {
+      "Content-Type": "application/json"
+    },
+
+    // Cos de la petició (les dades que enviem)
+    // Convertim l’objecte JS a text JSON
+    body: JSON.stringify({ data: text })
+  });
+
+  // El servidor respon amb JSON
+  // Convertim la resposta a objecte JavaScript
+  const json = await res.json();
+  
+  // Mostrem el resultat a la textarea de sortida
+  document.getElementById("output").value = json.result;
+});
